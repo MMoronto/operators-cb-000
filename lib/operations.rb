2cebf5752fe09b455e6c47354b88d347f53d2413
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-01-17 17:12:37 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,7 +3,7 @@ def unsafe?(speed)
     return "true"
   elsif speed < 40
     return "true"
-  elsif 40 > speed > 40 && speed < 60
+  elsif speed < 40 || speed > 60
     return "true"
   else
     return "false"
