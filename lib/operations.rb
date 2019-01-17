71c3382e5881fb28ca64b6ba606677f73f210390
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-01-17 17:16:31 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,17 @@
 def unsafe?(speed)
   if speed < 60 && speed > 40
-    return "true"
+    return true
   elsif speed < 40 || speed > 60
-    return "true"
+    return true
   else
-    return "false"
+    return false
   end  
 end
 
 
 
 def not_safe?(speed)
-	40 > speed || 60 < speed ? "true" : "false"
+	40 > speed || 60 < speed ? true : false
 end
 	
 
